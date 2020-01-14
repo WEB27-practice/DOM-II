f8965cd02833237656a41ec38108b632d2c7555c
seanaleid <seanaleid@gmail.com>
2020-01-14 12:07:06 -0500

    Adds initial code to start the daily project

---
+++ index.js
@@ -0,0 +1,28 @@
+/*Objectives:
+- Explain what events are in the browser window, and how event listeners can listen for those events.
+- add event listeners and event handlers to elements.
+- recognize event propagation and the method used to prevent it.
+*/
+
+// Step 1: Review Events
+
+/* psuedo code */
+// the element  the event  the code we want to run
+
+// mouse events 
+
+// click event 
+
+// double click event
+
+// resize event
+
+
+// step 2: Stop propogation
+
+// step 3: prevent default action
+
+
+
+
+// extra scroll event
