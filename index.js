0fb1dbd3cf6179c4e48e68db7777858b571b7b2a
seanaleid <seanaleid@gmail.com>
2020-01-14 13:05:23 -0500

    Adds more code for changing the image

--- index.js
+++ index.js
@@ -24,10 +24,26 @@ catImage.addEventListener('mouseleave', () => {
 })
 // click event 
 
+// example of how selecting multiple elements doesn't work with this pattern
+// const cardTitles = document.querySelectorAll(".card-title")
+
+// cardTitles.addEventListener("click", () => {
+//     carTitles.style.color = "hotpink";
+// })
+
 // double click event
-
+const changeImg = document.querySelector('.logo');
+changeImg.addEventListener("dblclick", () => {
+  changeImg.src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/780593/cat-lambda.png"
+})
 // resize event
+const dogsRule = document.querySelector(".card-img-top");
 
+window.addEventListener("resize", () => {
+  dogsRule.src = "https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+});
+
+console.log(window);
 
 // step 2: Stop propogation
 
